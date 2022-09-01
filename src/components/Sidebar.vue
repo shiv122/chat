8343c01877c2dbd11bfd4bbc9518a1d04e861b29
shiv122 <shivtiwari627@gmail.com>
2022-09-01 18:35:47 +0530

    adding swiper

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,6 +4,8 @@
       <profile />
       <chat />
       <group />
+      <contact />
+      <user-setting />
     </div>
   </div>
 </template>
@@ -12,4 +14,6 @@
 import Profile from "./tab-panel/Profile.vue";
 import Chat from "./tab-panel/Chat.vue";
 import Group from "./tab-panel/Group.vue";
+import Contact from "./tab-panel/Contact.vue";
+import UserSetting from "./tab-panel/UserSetting.vue";
 </script>
